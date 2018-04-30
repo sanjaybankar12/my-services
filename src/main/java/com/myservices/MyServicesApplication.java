97ca522127574ed55bdd967aaefe2315ade95e1a
Sanjay Bankar <sanjaybankar12@gmail.com>
2018-05-01 00:59:28 +0530

    initial commit

---
+++ src/main/java/com/myservices/MyServicesApplication.java
@@ -0,0 +1,12 @@
+package com.myservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/myservices/MyServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
